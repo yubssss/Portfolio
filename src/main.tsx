2e1a6ea36fba08a2c0e700757815fc6d918757a2
yubssss <jamescayube7@gmail.com>
2026-07-14 21:06:51 +0800

    new vercel

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
-import App from './App.js'
-import { ThemeProvider } from './context/ThemeContext.tsx'
+import App from './App'
+import { ThemeProvider } from './context/ThemeContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
